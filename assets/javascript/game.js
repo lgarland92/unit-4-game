8cf4f1f66f28aa8b53bab399e654f48ac35238b1
Lindsey Garland <rebeccgaupo@gmail.com>
2018-05-18 15:17:43 -0700

    got alerts to indicate win/loss, not running at right time

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ function game() {
 
     var wins = 0;
     var losses = 0;
-    
+
 
     $("#wins").html("Wins:" + wins);
 
@@ -12,64 +12,72 @@ function game() {
 
         var score = 0;
 
-        var goal = Math.floor(Math.random() * 31) + 50;
+        var goal = Math.floor(Math.random() * 100) + 20;
         console.log(goal);
 
-        var crystalOne = Math.floor(Math.random() * 20) + 1;
+        var crystalOne = Math.floor(Math.random() * 12) + 1;
         console.log(crystalOne);
 
-        var crystalTwo = Math.floor(Math.random() * 20) + 1;
+        var crystalTwo = Math.floor(Math.random() * 12) + 1;
         console.log(crystalTwo);
 
-        var crystalThree = Math.floor(Math.random() * 20) + 1;
+        var crystalThree = Math.floor(Math.random() * 12) + 1;
         console.log(crystalThree);
 
-        var crystalFour = Math.floor(Math.random() * 20) + 1;
+        var crystalFour = Math.floor(Math.random() * 12) + 1;
         console.log(crystalFour);
 
         $("#crystalOne").on("click", function () {
             score = score + crystalOne;
             console.log(score);
-            $("#score").html("Current Score: " +score + "Goal: " + goal);
+            $("#score").html("Current Score: " + score + "Goal: " + goal);
+            check();
         });
 
         $("#crystalTwo").on("click", function () {
             score = score + crystalTwo;
             console.log(score);
-            $("#score").html("Current Score: " +score + "Goal: " + goal);
+            $("#score").html("Current Score: " + score + "Goal: " + goal);
+            check();
         });
 
         $("#crystalThree").on("click", function () {
             score = score + crystalThree;
             console.log(score);
-            $("#score").html("Current Score: " +score + "Goal: " + goal);
+            $("#score").html("Current Score: " + score + "Goal: " + goal);
+            check();
         });
 
         $("#crystalFour").on("click", function () {
             score = score + crystalFour;
             console.log(score);
-            $("#score").html("Current Score: " +score + "Goal: " + goal);
+            $("#score").html("Current Score: " + score + "Goal: " + goal);
+            check();
         });
 
-    };
-
-    if (score === round.goal) {
-        wins++;
-
-        alert("You win!");
-
-        round();
-    };
-
-    if (score > round.goal) {
-        losses++;
-
-        alert("You lose!");
-
-        round();
+        
 
     };
 
+    function check() {
+        if (round.score === round.goal) {
+            wins++;
+
+            alert("You win!");
+
+            round();
+        };
+
+        if (round.score > round.goal) {
+            losses++;
+
+            alert("You lose!");
+
+            round();
+
+        };
+    }
+    
     round();
 };
 
